cdcf3c2ac9ae6f222cf3a52b549e8ebef5eb4b71
XodiumLuma <28709874+xodiumluma@users.noreply.github.com>
2026-09-21 19:14:57 +1000

    [learn] python setup WIP

--- __learn__/setup.py
+++ __learn__/setup.py
@@ -11,6 +11,20 @@ from fnmatch import fnmatchcase
 from pathlib import Path
 from shutil import copyfile, rmtree, move
 from setuptools import setui, find_namespace_packages, Extension, Command
+from setuptools.command.build_ext import build_ext
+from setuptools.command.build_clib import build_clib
+from setuptools.command.install import install
+from setuptools.command.build import build
+from setuptools.command.bdist_wheel import bdist_wheel
+from setuptools.errors import SetupError
 
 WHEEL_PACKAGE_DIR = "openvino"
 WHEEL_LIBS_INSTALL_DIR = f"{WHEEL_PACKAGE_DIR}/libs"
+WHEEL_LIBS_PACKAGE = "openvino.libs"
+
+suffix = "t" if hasattr(sys, "_is_gil_enabled") and not sys._is_gil_enabled() else ""
+PYTHON_VERSION = f"python{sys.version_info.major}.{sys.version_info.minor}{suffix}"
+
+LIBS_DIR = "bin" if platform.system() == "Windows" else "lib"
+
+machine = platform.machine()
