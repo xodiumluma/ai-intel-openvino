5e525c9cdec39ca13684a5e30ef63e9038890514
XodiumLuma <28709874+xodiumluma@users.noreply.github.com>
2026-09-21 15:18:00 +1000

    [learn] python setup WIP

--- __learn__/setup.py
+++ __learn__/setup.py
@@ -8,3 +8,9 @@ import shutil
 import multiprocessing
 import logging as log
 from fnmatch import fnmatchcase
+from pathlib import Path
+from shutil import copyfile, rmtree, move
+from setuptools import setui, find_namespace_packages, Extension, Command
+
+WHEEL_PACKAGE_DIR = "openvino"
+WHEEL_LIBS_INSTALL_DIR = f"{WHEEL_PACKAGE_DIR}/libs"
