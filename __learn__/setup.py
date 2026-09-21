2ebfed897a0f8d6e6622a12d5a4f9ed9f0a1821f
XodiumLuma <28709874+xodiumluma@users.noreply.github.com>
2026-09-21 15:01:02 +1000

    [learn] python setup WIP

--- __learn__/setup.py
+++ __learn__/setup.py
@@ -2,3 +2,9 @@ import os.path
 import sys
 import errno
 import subprocess  # nosec
+import platform
+import re
+import shutil
+import multiprocessing
+import logging as log
+from fnmatch import fnmatchcase
